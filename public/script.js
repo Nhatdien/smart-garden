80301d9eb71f0a2a472043518c15d25271982a19
Nhatdien <nhatkhonvl@gmail.com>
2024-12-16 23:17:15 +0700

    Update sensor data values and enhance data fetching logic for gauges

--- public/script.js
+++ public/script.js
@@ -4,9 +4,9 @@ async function fetchData() {
     const data = await res.json();
   
     // Update gauges
-    document.getElementById('temperature-gauge').innerText = `${data.temperature[0]} °C`;
-    document.getElementById('humidity-gauge').innerText = `${data.humidity[0]} %`;
-    document.getElementById('last-update').innerText = data.timestamps[0];
+    document.getElementById('temperature-gauge').innerText = `${data.temperature?.[data.temperature?.length - 1] || `N/A`} °C`;
+    document.getElementById('humidity-gauge').innerText = `${data.humidity[data.humidity.length - 1] || `N/A`} %`;
+    document.getElementById('last-update').innerText = data.timestamps[0] || 'N/A';
   
     renderChart('lightChart', 'Light Intensity', data.light);
     renderChart('distanceChart', 'Distance', data.distance);
